dced6c302ae69eb703a5a633ec94405574df8420
Vincent Thai <vincentthai@live.ca>
2023-06-01 13:08:12 -0400

    Added banner, imported PIL to resize and process images

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+
 from tkinter import *
 from tkinter import Tk
 from tkinter import filedialog
+
 from pygame import mixer
+
+from PIL import Image, ImageTk
 
 # Initalize window for the music player
 root = Tk()
@@ -12,12 +16,22 @@ root.configure(background='#212121')
 
 
 label = Label(root, text="Testing")
-label.pack()
+label.pack(side = BOTTOM)
 
-# Sets the iconphoto
-spotify_logo = PhotoImage(file="Images\spotify_logo.png")
-root.iconphoto(False, spotify_logo)
+# Sets the Icon photo
+spotify_iconlogo = PhotoImage(file="Images\spotify_logo.png")
+root.iconphoto(False, spotify_iconlogo)
 
+
+# Loads the image
+wide_logo = Image.open(r"Images\spotify_logo_wide.png")
+
+# Resizes, and sets the banner photo
+wide_logo = wide_logo.resize((400,225))
+spotify_widelogo = ImageTk.PhotoImage(wide_logo)
+Label(root, image = spotify_widelogo).pack(anchor = "n", side = "left")
+
+
+# Runs the application
 root.mainloop()
 
-mixer.init()
