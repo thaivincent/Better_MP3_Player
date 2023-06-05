e0fca487feefeec24a436002e7afeb2664e6141b
Vincent <vincentthai@live.ca>
2023-06-05 10:25:11 -0400

    Fixed Relative path for images on Linux systems

--- main.py
+++ main.py
@@ -19,17 +19,18 @@ label = Label(root, text="Testing")
 label.pack(side = BOTTOM)
 
 # Sets the Icon photo
-spotify_iconlogo = PhotoImage(file="Images\spotify_logo.png")
+spotify_iconlogo = PhotoImage(file="Images/spotify_logo.png")
 root.iconphoto(False, spotify_iconlogo)
 
 
 # Loads the image
-wide_logo = Image.open(r"Images\spotify_logo_wide.png")
+wide_logo = Image.open(r"Images/spotify_logo_wide.png")
 
 # Resizes, and sets the banner photo
 wide_logo = wide_logo.resize((400,225))
 spotify_widelogo = ImageTk.PhotoImage(wide_logo)
 Label(root, image = spotify_widelogo).pack(anchor = "n", side = "left")
+
 
 
 # Runs the application
