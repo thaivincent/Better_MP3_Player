a3bf4ccfec5f7fea2ce24938d36f3470334fb6f9
Vincent Thai <vincentthai@live.ca>
2023-09-16 17:50:06 -0400

    Implemented a song list to appear after folder selection

--- main.py
+++ main.py
@@ -23,21 +23,24 @@ mixer.init()
 # Create a function to select a music directory
 def SelectFolder():
     # Asks the user to select a directory
-    filepath = filedialog.askdirectory(initialdir=r"C:\Users\vince\VS Code\Better_MP3_Player", title="Dialog Box")
+    folder_path = filedialog.askdirectory(initialdir=r"C:\Users\vince\VS Code\Better_MP3_Player", title="Dialog Box")
 
     # Displays the appropriate directory
-    Label(root, text = filepath).place(x=50, y=700)
+    Label(root, text = folder_path).place(x=50, y=700)
 
-def PlayMusic():
-    Music_Name = Playlist.get(ACTIVE)
-    print(Music_Name[0:-4])
-    mixer.music.load(Playlist.get(ACTIVE))
-    mixer.music.play()
+    song_listbox = Listbox(root, height= 40, width = 100, selectmode=SINGLE)
+    song_list = os.listdir(folder_path)
+    index = 0
+    for i in song_list:
+        song_listbox.insert(index,i)
+        index += 1
+
+    song_listbox.place(x = 450, y = 50)   
+
 
 # Sets the Icon photo
 spotify_iconlogo = PhotoImage(file="Images/spotify_logo.png")
 root.iconphoto(False, spotify_iconlogo)
-
 
 # Loads the image
 wide_logo = Image.open(r"Images/spotify_logo_wide.png")
@@ -50,14 +53,12 @@ Label(root, image = spotify_widelogo).pack(anchor = "n", side = "left")
 # Initializing the pictures for the buttons
 file_image = Image.open(r"Images/file_logo.png")
 file_image = file_image.resize((200,200))
-button_Add = ImageTk.PhotoImage(file_image)    
-Button(root, image=button_Add, bg="#0f1a2b",bd=0, command=SelectFolder).place(x=50, y=500)
-
+button_File = ImageTk.PhotoImage(file_image)    
+Button(root, image=button_File, bg="#0f1a2b",bd=0, command = SelectFolder).place(x=50, y=500)
 
 button_Resume = PhotoImage(file="Images/resume_button.png")
 
 button_Pause = PhotoImage(file="Images/pause_button.png")
-
 
 
 # Runs the application
