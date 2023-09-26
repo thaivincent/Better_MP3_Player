156b056ef781c4748728ccfbfe52b564b4619047
Vincent Thai <vincentthai@live.ca>
2023-09-25 22:07:56 -0400

    Added a list box to display songs, and a play music function

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ root = Tk()
 root.title("Music Player")
 root.geometry("920x600+290+85")
 root.configure(background='#212121')
-mixer.init()    
+mixer.init()
 
 # Create a function to select a music directory
 def SelectFolder():
     # Asks the user to select a directory
+    global folder_path
     folder_path = filedialog.askdirectory(initialdir=r"C:\Users\vince\VS Code\Better_MP3_Player", title="Dialog Box")
 
     # Displays the appropriate directory
     Label(root, text = folder_path).place(x=50, y=700)
 
-    song_listbox = Listbox(root, height= 40, width = 100, selectmode=SINGLE)
+    #Initialize a listbox
+    global song_listbox
+    song_listbox = Listbox(root, height= 20, width = 100, selectmode=SINGLE)
+
+    # Loop through the elements in the list to add them to the listbox
     song_list = os.listdir(folder_path)
     index = 0
     for i in song_list:
@@ -36,6 +41,14 @@ def SelectFolder():
         index += 1
 
     song_listbox.place(x = 450, y = 50)   
+
+
+def PlayMusic():
+    for i in song_listbox.curselection():
+        song_file = song_listbox.get(i)
+        mixer.music.load(folder_path + "/" + song_file)
+        
+
 
 
 # Sets the Icon photo
@@ -59,6 +72,14 @@ Button(root, image=button_File, bg="#0f1a2b",bd=0, command = SelectFolder).place
 button_Resume = PhotoImage(file="Images/resume_button.png")
 
 button_Pause = PhotoImage(file="Images/pause_button.png")
+
+
+# Place the play music button
+
+play_image = Image.open(r"C:\Users\vince\VS Code\Better_MP3_Player\Images\resume_button.png")
+play_image = play_image.resize((100,100))
+spotify_play_image = ImageTk.PhotoImage(play_image)
+Button(root, image = spotify_play_image, command = PlayMusic).place(x=460, y = 550)
 
 
 # Runs the application
